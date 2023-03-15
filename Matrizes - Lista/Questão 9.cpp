2296eaf4137a806af34f29acbca5fcba74aca34a
Erivandobezerra <105008622+Erivandobezerra@users.noreply.github.com>
2023-03-15 09:09:55 -0300

    Update Questão 9.cpp

--- Matrizes - Lista/Questão 9.cpp
+++ Matrizes - Lista/Questão 9.cpp
@@ -7,32 +7,32 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese_Brazil");
 	
-	int matriz_original[LIN][COL];
+	int m_o[LIN][COL];
 	int ib = 0, jb =  0;
 	int R[LIN][COL];
 
 	for(int i = 0; i < LIN; i++){
 		for(int j = 0; j < COL; j++){
-			printf("Digite o valor da posição [%d][%d]: ", i, j);
-			scanf("%d", &matriz_original[i][j]);
+			printf("Digite o valor da posiÃ§Ã£o [%d][%d]: ", i, j);
+			scanf("%d", &m_o[i][j]);
 		}
 	}
 	
-	printf("\nMatriz Original:\n");
+	printf("\n A Matriz Original:\n");
 	for(int i = 0; i < LIN; i++){
 		for(int j = 0; j < COL; j++){
-			printf("%d\t", matriz_original[i][j]);
+			printf("%d\t", m_o[i][j]);
 		}
 		printf("\n");
 	}
 	
 	
-	printf("\nMatriz na rotação 90 graus:\n");
+	printf("\nA Sua Matriz na rotaÃ§Ã£o 90 graus:\n");
 	for(int j = 0; j < COL; j++){
 		jb = 0;
 		for(int i = LIN - 1; i >= 0; i--){
-			R[ib][jb] = matriz_original[i][j];
-			printf("%d \t", matriz_original[i][j]);	
+			R[ib][jb] = m_o[i][j];
+			printf("%d \t", m_o[i][j]);	
 			jb++;
 		}
 		printf("\n");
